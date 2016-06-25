7a6fb9d075c582066217b538c577fd8c88f000cc
Wayne Goh <wayneangoh@gmail.com>
2016-06-25 19:40:43 +0800

    rspec file works

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,37 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+
+helper_method :current_user
+helper_method :user_logged_in?
+
+def current_user
+  @c_user ||= User.find( session[:user_id] ) if session[:user_id]
+  # @c_user ||= User.find( session[:user_id] ) unless session[:user_id].blank?
+
+  # User.find( session[:user_id] ) unless session[:user_id].blank?
+
+# if @c_user.present?
+#   return @c_user
+# elsif session[:user_id].present?
+#   @c_user = User.find( session[:user_id] )
+#   return @c_user
+# else
+#   return nil
+# end
+
+end
+
+
+
+def user_logged_in?
+  current_user.present?
+end
+
+def authenticate_user
+  unless user_logged_in?
+    flash[:error] = "you must login first"
+    redirect_to login_url
+  end
+end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  has_many :posts
+  has_secure_password
+  validates :email, presence: true, uniqueness: true, format: { with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/, message: ': Your Email Address Is Not Valid' }
+  validates_length_of :password, minimum: 8
+end
