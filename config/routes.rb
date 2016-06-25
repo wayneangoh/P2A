7492a69dd870c40dad1e071843735f656ec509ec
Wayne Goh <wayneangoh@gmail.com>
2016-06-26 00:12:55 +0800

    change migration file

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :users
 
   get 'login', to: 'sessions#new'
+  delete 'logout', to: 'sessions#destroy'
   resources :sessions, only: [:create]
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -18,10 +18,10 @@ ActiveRecord::Schema.define(version: 20160624105606) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.integer  "users_id"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["users_id"], name: "index_posts_on_users_id", using: :btree
+    t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,5 +31,5 @@ ActiveRecord::Schema.define(version: 20160624105606) do
     t.datetime "updated_at",      null: false
   end
 
-  add_foreign_key "posts", "users", column: "users_id"
+  add_foreign_key "posts", "users"
 end
