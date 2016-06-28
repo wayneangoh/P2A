7268fb5b8a85d5b8580513656448458e03f9b0a0
Wayne Goh <wayneangoh@gmail.com>
2016-06-28 17:36:38 +0800

    added bootstrap

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
+  # before_save { email.downcase! }
   has_many :posts
   has_secure_password
-  validates :email, presence: true, uniqueness: true, format: { with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/, message: ': Email is invalid' }
-  validates_length_of :password, minimum: 8
+  validates :email, presence: true, uniqueness: { case_sensitive: true }, format: { with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/, message: ': Email is invalid' }
+  validates :password, presence: true, length: { minimum: 8 }
 end
