1e0d093d4a57510d61feb1cbd59e3221531374da
Wayne Goh <wayneangoh@gmail.com>
2016-06-29 17:42:33 +0800

    bootstrap integrated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.paginate(page: params[:page], per_page: 2)
+    @users = User.all
   end
 
   # GET /users/1
